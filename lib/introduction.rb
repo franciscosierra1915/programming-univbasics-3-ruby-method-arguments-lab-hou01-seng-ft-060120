df6d391695c8445337fb4c606f5516ccfce72735
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-12 21:55:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,8 @@ def introduction(name)
   puts "Hi my name is #{name}."
 end
 
+introduction("Fran")
+
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
